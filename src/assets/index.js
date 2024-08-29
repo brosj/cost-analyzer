27f41cec510abe3a5a70852ff697dff6c11cb8ba
brosj <joshuaonyeuche@gmail.com>
2024-08-29 15:56:26 +0100

    Add assets

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import Bell from "../assets/Notification Bell.svg";
+import Calendar from "../assets/calendar.svg";
+import ChevronDown from "../assets/ChevronDown.svg";
+import Logo from "../assets/Logo_CA.svg";
+import Search from "../assets/Search.svg";
+import Snowflake from "../assets/Snowflake Logo.svg";
+import Star from "../assets/Star.svg";
+import StarChecked from "../assets/Star_checked.svg";
+import StarUnchecked from "../assets/Star_unchecked.svg";
+
+export {
+  Bell,
+  Calendar,
+  ChevronDown,
+  Logo,
+  Search,
+  Snowflake,
+  Star,
+  StarChecked,
+  StarUnchecked,
+};
